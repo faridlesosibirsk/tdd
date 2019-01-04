67ceaae94033f8bcd108ab0a2b0e558a8958f1e4
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2019-01-04 21:41:19 +0700

    EqualityForAll

--- models/Dollar.php
+++ models/Dollar.php
@@ -14,5 +14,9 @@ class Dollar {
         //$this->amount = $this->amount * 2;
         return new Dollar($this->amount * $multiplier);
     }
+    public function equals(Object $object){
+        $dollar= $object;
+        return $this->amount == $dollar->amount;
+    }
 
 }
---
+++ tests/unit/EqualityTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use app\models\Dollar;
+
+class EqualityTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testEquality()
+    {
+        $this->assertTrue((new Dollar(5))->equals(new Dollar(5)));
+        $this->assertFalse((new Dollar(5))->equals(new Dollar(6)));
+    }
+}
